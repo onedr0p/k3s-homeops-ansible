36fe40fc69bf1c7dd2071958b0d22e5499ced2d9
Ryan Holt <ryan@ryanholt.net>
2020-02-14 19:58:17 +0000

    remove dynamic attempt at locating repo
    Signed-off-by: Ryan Holt <ryan@ryanholt.net>

--- scripts/add-repo-key.sh
+++ scripts/add-repo-key.sh
@@ -24,6 +24,13 @@ else
   KEY="$2"
 fi
 
+if [ -z "$3" ]; then
+  echo "This script requires a repo name passed in as an argument"
+  exit 1
+else
+  reponame="$3"
+fi
+
 need() {
     which "$1" &>/dev/null || die "Binary '$1' is missing but required"
 }
@@ -34,46 +41,32 @@ need "jq"
 
 github_access_token=$(cat "$HOME"/.secret/github_access_token)
 
-rp=$(git rev-parse --is-inside-work-tree 2>/dev/null)
+repo_id="github-deploy-$reponame.github.com"
 
-if [ "$?" -eq 0 ] && [ "$rp" = "true" ]; then
-    url="$(git config --get remote.origin.url)"
-    repo=${url##*/}
-    reponame=${repo%%.*}
-
-    repo_id="github-deploy-$reponame.github.com"
-
-    # delete all existing deploy keys
-    curl \
-        -H"Authorization: token $github_access_token"\
-        https://api.github.com/repos/"$github_username"/"$reponame"/keys 2>/dev/null\
-        | jq '.[] | .id ' | \
-        while read _id; do
-            echo "- deploy key: $_id"
-            curl \
-                -X "DELETE"\
-                -H"Authorization: token $github_access_token"\
-                https://api.github.com/repos/"$github_username"/"$reponame"/keys/"$_id" 2>/dev/null
-        done
-
-    # add the keyfile to github
-    echo
-    echo "+ deploy key:"
-    echo -n ">> "
-    {
+# delete all existing deploy keys
+curl \
+    -H"Authorization: token $github_access_token"\
+    https://api.github.com/repos/"$github_username"/"$reponame"/keys 2>/dev/null\
+    | jq '.[] | .id ' | \
+    while read _id; do
+        echo "- deploy key: $_id"
         curl \
-            -i\
+            -X "DELETE"\
             -H"Authorization: token $github_access_token"\
-            --data @- https://api.github.com/repos/"$github_username"/"$reponame"/keys << EOF
-    {
-        "title" : "$repo_id $(date)",
-        "key" : "$KEY",
-        "read_only" : false
-    }
+            https://api.github.com/repos/"$github_username"/"$reponame"/keys/"$_id" 2>/dev/null
+    done
+# add the keyfile to github
+echo
+echo "+ deploy key:"
+echo -n ">> "
+{
+    curl \
+        -i\
+        -H"Authorization: token $github_access_token"\
+        --data @- https://api.github.com/repos/"$github_username"/"$reponame"/keys << EOF
+{
+    "title" : "$repo_id $(date)",
+    "key" : "$KEY",
+    "read_only" : false
+}
 EOF
-    } 2>/dev/null | head -1 # status code should be 201
-
-else
-    echo 'Not a git repository'
-    exit
-fi
