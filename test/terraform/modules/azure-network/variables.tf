26b2434248c3a482b4aa2e5d9192f059372d0945
Ryan Holt <ryan@ryanholt.net>
2020-02-18 09:23:18 -0500

    whitespace / formatting
    Signed-off-by: Ryan Holt <ryan@ryanholt.net>

--- test/terraform/modules/azure-network/variables.tf
+++ test/terraform/modules/azure-network/variables.tf
@@ -1,29 +1,29 @@
 variable "location" {
   description = "Azure Location for resources to be deployed to"
-  type = string
+  type        = string
 }
 
 variable "rg_name" {
   description = "Resource Group containing all deployed objects"
-  type = string
+  type        = string
 }
 
 variable "vnet_address_space" {
   description = "Address Spaces for vNET"
-  type = list(string)
+  type        = list(string)
 }
 
 variable "vnet_name" {
   description = "vNet Name"
-  type = string
+  type        = string
 }
 
 variable "vnet_subnet_name" {
   description = "Subnet Name"
-  type = string
+  type        = string
 }
 
 variable "vnet_subnet_prefix" {
   description = "Subnet Prefix"
-  type = string
+  type        = string
 }
