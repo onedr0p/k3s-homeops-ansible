e50a2badba77f83a8c0bd9ea21d506e3dac8f0c4
Ryan Holt <ryan@ryanholt.net>
2020-02-21 09:32:25 -0500

    move all files to cluster folder
    Signed-off-by: Ryan Holt <ryan@ryanholt.net>

--- hack/kvault.sh
+++ hack/kvault.sh
@@ -4,6 +4,6 @@ export REPO_ROOT=$(git rev-parse --show-toplevel)
 
 name="secrets/$(dirname "$@")/$(basename -s .txt "$@")"
 echo "Writing $name to vault"
-if output=$(envsubst < "$REPO_ROOT/devops/$*"); then
+if output=$(envsubst < "$REPO_ROOT/cluster/$*"); then
   printf '%s' "$output" | vault kv put "$name" values.yaml=-
 fi
